53ce302b9ed674e7e94763b37d93606251a371ed
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2025-11-06 10:13:28 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@0722ef1ae6e592158a7fcf91a3f4fb40cc366439 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/long/";
             },},{id: "news-press-release-from-our-collaborative-work",
           title: 'press release from our collaborative work',
           description: "",
