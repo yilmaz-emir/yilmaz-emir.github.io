9d061219f6a5993fe9c62ffeed8a68a134cd595a
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2026-03-12 05:26:45 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@22f9a5145187c7bcdb4a63b7be4efa5b55965a13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,6 +436,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_10/";
+            },},{id: "news-six-presentations-at-icatye-2025-2026",
+          title: 'Six presentations at ICATYE 2025-2026!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_11/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
