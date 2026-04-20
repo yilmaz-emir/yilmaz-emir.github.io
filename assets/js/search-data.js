2b118f2c1728eb2accf1c83d94c494a8db42e512
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2026-04-20 01:50:32 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@43e7528c9725823582a93f415f427574a45cdd61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,6 +436,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_10/";
+            },},{id: "news-outstanding-research-poster-award-jspe-2026-spring-conference",
+          title: 'Outstanding Research Poster Award @JSPE 2026 Spring Conference',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_12/";
             },},{id: "news-six-presentations-at-icatye-2025-2026",
           title: 'Six presentations at ICATYE 2025-2026!',
           description: "",
