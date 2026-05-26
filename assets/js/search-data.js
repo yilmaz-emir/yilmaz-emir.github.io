f2e83bab8a2252a5bd01dd66c961ed20c22e1f10
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2026-05-26 07:54:20 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@573078853b0eb676758e0de3a40ca46ef9911274 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,8 +446,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_12/";
-            },},{id: "news-oral-presentation-at-the-51st-leeds-lyon-sympoisum-on-tribology",
-          title: 'Oral presentation at the 51st Leeds-Lyon Sympoisum on Tribology',
+            },},{id: "news-acceptance-oral-presentation-at-the-51st-leeds-lyon-sympoisum-on-tribology",
+          title: 'Acceptance! Oral presentation at the 51st Leeds-Lyon Sympoisum on Tribology',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_13/";
