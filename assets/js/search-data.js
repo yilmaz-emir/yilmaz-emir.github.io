fcebd142008676c601d8075cdddf311abf9e094b
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2026-09-24 07:15:18 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@620cd99c4004626c7a73dd5c6e9185ae464f134b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -528,6 +528,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=R2D30vAAAAAJ", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/PERG_YilmazLab", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
